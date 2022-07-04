482bb8e97a9ae54a396fcef37269f7774de955df
Lucas-Jiang-9723 <yj1084@uchicago.edu>
2022-07-04 19:51:26 +0000

    final commit - tidy version 1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,14 +8,20 @@ class ApplicationController < ActionController::Base
   end
   
   def play_rock
+    @cpu_move = ["rock", "paper", "scissors"].sample
+    @result_dict = {"rock"=>"tied" , "paper"=>"lost", "scissors"=>"won"}
     render({:template => "game_templates/user_rock.html.erb"})
   end
 
   def play_scissors
+    @cpu_move = ["rock", "paper", "scissors"].sample
+    @result_dict = {"rock"=>"lost" , "paper"=>"won", "scissors"=>"tied"}
     render({:template => "game_templates/user_scissors.html.erb"})
   end
 
   def play_paper
+    @cpu_move = ["rock", "paper", "scissors"].sample
+    @result_dict = {"rock"=>"won" , "paper"=>"tied", "scissors"=>"lost"}
     render({:template => "game_templates/user_paper.html.erb"})
   end
 
