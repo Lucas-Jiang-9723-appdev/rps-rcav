4246f182c555126faaffd7d3df5170a5301fcd70
Lucas-Jiang-9723 <yj1084@uchicago.edu>
2022-07-04 19:33:11 +0000

    complete ver_1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,21 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
+  def homepage
+    render({:template => "game_templates/homepage.html.erb"})
+  end
+  
   def play_rock
     render({:template => "game_templates/user_rock.html.erb"})
   end
+
+  def play_scissors
+    render({:template => "game_templates/user_scissors.html.erb"})
+  end
+
+  def play_paper
+    render({:template => "game_templates/user_paper.html.erb"})
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get("/", {:controller => "application", :action => "homepage"})
   get("/rock", {:controller => "application", :action => "play_rock"})
-
+  get("/scissors", {:controller => "application", :action => "play_scissors"})
+  get("/paper",{:controller => "application", :action => "play_paper"})
 end
